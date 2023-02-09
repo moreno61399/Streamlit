8b220d33c717559f22c0b910cc556c4c35970f96
Eduardo <moreno.61399@gmail.com>
2023-02-09 18:10:39 +0100

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,10 +1,11 @@
 import streamlit as st
+from openpyxl import reader,load_workbook
 
 st.title("FK_KM CHECK")
 
 fkonzept = st.file_uploader("upload FK file", type={"xlsx","csv", "txt"})
 
-wb=workbook()
+wb=Workbook()
 
 if fkonzept is not None:
     wb =load_workbook(fkonzept, read_only=True)
