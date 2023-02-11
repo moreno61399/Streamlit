b8c8abe2afec6dae224321c6409dc491c1d93037
Eduardo <moreno.61399@gmail.com>
2023-02-11 11:57:17 +0100

    Update streamlit_FK_Check.py

--- streamlit_FK_Check.py
+++ streamlit_FK_Check.py
@@ -183,7 +183,7 @@ df_km=pd.DataFrame()
 if fkonzept is not None:
     #df_fk = pd.read_excel(fkonzept)
     #st.write(df_fk)
-    wb =load_workbook(fkonzept, read_only=False)
+    wb =load_workbook(fkonzept, read_only=False,data_only=True)
     #dict_vts = get_dict_vts(wb, '', "yes")
     #st.write(dict_vts)
     #st.write(wb.sheetnames)
